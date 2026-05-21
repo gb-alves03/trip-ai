dbf096e4a7c2243dbdd077911168c6805ccd5eff
gb-alves03 <gabrielhalves03@gmail.com>
2026-05-21 14:20:46 -0300

    feat: initial commit

---
+++ src/main/java/com/trip_ai/TripAiApplication.java
@@ -0,0 +1,13 @@
+package com.trip_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trip_ai/TripAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
